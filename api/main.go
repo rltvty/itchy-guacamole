df66cb6864f600811ea78ca33f7a54d0dc7b6cd8
Ryan Michael <kerinin@gmail.com>
2015-10-28 13:48:57 -0500

    meh

--- api/main.go
+++ api/main.go
@@ -19,6 +19,25 @@ const (
 	cardsKey = iota
 )
 
+// Card describes a dominion card
+type Card struct {
+	CostPotions   int    `json:"cost_potions"`
+	CostTreasure  int    `json:"cost_treasure"`
+	Description   string `json:"description"`
+	Expansion     string `json:"expansion"`
+	ID            int    `json:"id"`
+	IsAttack      bool   `json:"is_attack"`
+	IsReaction    bool   `json:"is_reaction"`
+	Name          string `json:"name"`
+	PlusActions   int    `json:"plus_actions"`
+	PlusBuys      int    `json:"plus_buys"`
+	PlusCards     int    `json:"plus_cards"`
+	PlusTreasure  int    `json:"plus_treasure"`
+	Trashes       int    `json:"trashes"`
+	Treasure      int    `json:"treasure"`
+	VictoryPoints int    `json:"victory_points"`
+}
+
 func makeDeck(ctx context.Context, w http.ResponseWriter, r *http.Request) {
 	cards, _ := ctx.Value(cardsKey).([]Card)
 
@@ -29,7 +48,10 @@ func makeDeck(ctx context.Context, w http.ResponseWriter, r *http.Request) {
 	}
 
 	enc := json.NewEncoder(w)
-	enc.Encode(deck)
+	_ = enc.Encode(deck)
+
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
 }
 
 func main() {
@@ -40,7 +62,11 @@ func main() {
 	}
 
 	var cards []Card
-	json.Unmarshal(file, &cards)
+	err := json.Unmarshal(file, &cards)
+	if err != nil {
+		fmt.Printf("JSON Decode error: %v\n", err)
+		os.Exit(1)
+	}
 
 	ctx := context.Background()
 	ctx = context.WithValue(ctx, cardsKey, cards)
