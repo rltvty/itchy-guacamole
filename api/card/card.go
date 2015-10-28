e26ea74459edaa5d407ca313b09f4913419e0277
David Lyons <david.lyons@returnpath.com>
2015-10-28 15:11:18 -0500

    reorg 2

---
+++ api/card/card.go
@@ -0,0 +1,25 @@
+package card
+
+// Card describes a dominion card
+type Card struct {
+	CostPotions   int    `json:"cost_potions"`
+	CostTreasure  int    `json:"cost_treasure"`
+	Description   string `json:"description"`
+	Event         bool   `json:"event"`
+	Expansion     string `json:"expansion"`
+	ID            int    `json:"id"`
+	IsAttack      bool   `json:"is_attack"`
+	IsReaction    bool   `json:"is_reaction"`
+	Name          string `json:"name"`
+	PlusActions   int    `json:"plus_actions"`
+	PlusBuys      int    `json:"plus_buys"`
+	PlusCards     int    `json:"plus_cards"`
+	PlusTreasure  int    `json:"plus_treasure"`
+	Trashes       int    `json:"trashes"`
+	Treasure      int    `json:"treasure"`
+	VictoryPoints int    `json:"victory_points"`
+}
+
+const (
+	Key = iota
+)
