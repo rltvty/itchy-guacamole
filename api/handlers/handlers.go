c8f9f6e1b302c3be2dae0e347032939a691b9297
Ryan Michael <kerinin@gmail.com>
2015-10-29 14:18:36 -0500

    Organize a bit

--- api/handlers/handlers.go
+++ api/handlers/handlers.go
@@ -16,19 +16,26 @@ import (
 
 var rnd = rand.New(rand.NewSource(time.Now().UnixNano()))
 
-type deckResponse struct {
-	deck.Deck
-	Potions            bool `json:"potions"`
+type deckHardware struct {
 	CoinTokens         bool `json:"coin_tokens"`
 	VictoryTokens      bool `json:"victory_tokens"`
-	TavernMats         bool `json:"tavern_mats"`
-	TradeRouteMats     bool `json:"trade_route_mats"`
-	NativeVillageMats  bool `json:"native_village_mats"`
-	Spoils             bool `json:"spoils"`
-	Ruins              bool `json:"ruins"`
 	MinusOneCardTokens bool `json:"minus_one_card_tokens"`
 	MinusOneCoinTokens bool `json:"minus_one_coin_tokens"`
 	JourneyTokens      bool `json:"journey_tokens"`
+	TavernMats         bool `json:"tavern_mats"`
+	TradeRouteMats     bool `json:"trade_route_mats"`
+	NativeVillageMats  bool `json:"native_village_mats"`
+}
+
+type deckResponse struct {
+	Cards                []Card       `json:"cards"`
+	Events               []Card       `json:"events"`
+	ColoniesAndPlatinums bool         `json:"colonies_and_platinums"`
+	Shelters             bool         `json:"shelters"`
+	Potions              bool         `json:"potions"`
+	Spoils               bool         `json:"spoils"`
+	Ruins                bool         `json:"ruins"`
+	Hardware             deckHardware `json:"hardware"`
 }
 
 func makeDeck(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
@@ -57,18 +64,23 @@ func makeDeck(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	}
 
 	resp := deckResponse{
-		Deck:               d,
-		Potions:            d.Potions(),
-		CoinTokens:         d.CoinTokens(),
-		VictoryTokens:      d.VictoryTokens(),
-		TavernMats:         d.TavernMats(),
-		TradeRouteMats:     d.TradeRouteMats(),
-		NativeVillageMats:  d.NativeVillageMats(),
-		Spoils:             d.Spoils(),
-		Ruins:              d.Ruins(),
-		MinusOneCardTokens: d.MinusOneCardTokens(),
-		MinusOneCoinTokens: d.MinusOneCoinTokens(),
-		JourneyTokens:      d.JourneyTokens(),
+		Cards:                d.Cards,
+		Events:               d.Events,
+		ColoniesAndPlatinums: d.ColoniesAndPlatinums,
+		Shelters:             d.Shelters(),
+		Potions:              d.Potions(),
+		Spoils:               d.Spoils(),
+		Ruins:                d.Ruins(),
+		Hardware: deckHardware{
+			CoinTokens:         d.CoinTokens(),
+			VictoryTokens:      d.VictoryTokens(),
+			MinusOneCardTokens: d.MinusOneCardTokens(),
+			MinusOneCoinTokens: d.MinusOneCoinTokens(),
+			JourneyTokens:      d.JourneyTokens(),
+			TavernMats:         d.TavernMats(),
+			TradeRouteMats:     d.TradeRouteMats(),
+			NativeVillageMats:  d.NativeVillageMats(),
+		},
 	}
 	enc := json.NewEncoder(w)
 	_ = enc.Encode(resp)
