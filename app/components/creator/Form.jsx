2044569ea339086c5412ed0165e203fb68c8ce1a
AaronLaBrie <aaron@aaronlabrie.com>
2015-10-30 13:58:28 -0500

    Available sets from env

--- app/components/creator/Form.jsx
+++ app/components/creator/Form.jsx
@@ -4,59 +4,43 @@ var CheckboxGroup = require('./CheckboxGroup');
 module.exports = React.createClass({
   displayName: 'Form',
 
+  componentDidMount: function() {
+    $.get('/sets', function(data) {
+      data = JSON.parse(data);
+      this.setState({ availableSets: data });
+    }.bind(this)).fail(function(err) {
+      console.error(err);
+    });
+  },
+
   getInitialState: function() {
-    return {selectedSets: ['dominion']};
+    return {selectedSets: [], availableSets: []};
   },
 
   handleChange: function() {
     var selectedSets = this.refs.setsGroup.getCheckedValues();
     this.setState({selectedSets: selectedSets});
+    console.log(this.state)
+  },
+
+  getSets: function() {
+    return this.state.availableSets.map(function(set, index) {
+      return (
+        <label>
+          <input type="checkbox" value={set}/>{set.split('_').join(' ')}
+        </label>
+      );
+    });
   },
 
   render: function() {
+    var sets = this.getSets();
+
     return(
       <div id='form'>
         <CheckboxGroup name="sets" value={this.state.selectedSets} ref="setsGroup" onChange={this.handleChange}>
         <div id='labels'>
-          <label>
-            <input type="checkbox" value="dominion"/>dominion
-          </label>
-          <label>
-            <input type="checkbox" value="intrigue"/>intrigue
-          </label>
-          <label>
-            <input type="checkbox" value="seaside"/>seaside
-          </label>
-          <label>
-            <input type="checkbox" value="alchemy"/>alchemy
-          </label>
-          <label>
-            <input type="checkbox" value="prosperity"/>prosperity
-          </label>
-          <label>
-            <input type="checkbox" value="cornucopia"/>cornucopia
-          </label>
-          <label>
-            <input type="checkbox" value="hinterlands"/>hinterlands
-          </label>
-          <label>
-            <input type="checkbox" value="dark_ages"/>dark ages
-          </label>
-          <label>
-            <input type="checkbox" value="guilds"/>guilds
-          </label>
-          <label>
-            <input type="checkbox" value="adventures"/>adventures
-          </label>
-          <label>
-            <input type="checkbox" value="governor"/>governor
-          </label>
-          <label>
-            <input type="checkbox" value="prince"/>prince
-          </label>
-          <label>
-            <input type="checkbox" value="summon"/>summon
-          </label>
+          {sets}
         </div>
       </CheckboxGroup>
       </div>
