cc0afbc9ef56f5c7af40687be086ef58b99f544e
Susi Rathmann <susi@context.io>
2015-10-30 15:41:33 -0500

    oops routing

--- api/handlers/routes.go
+++ api/handlers/routes.go
@@ -16,7 +16,7 @@ func SetRoutes() *httprouter.Router {
 	router.POST("/deck", basicAuth(makeDeck, user, pass))
 	router.GET("/deck/:id", basicAuth(getDeck, user, pass))
 	router.GET("/sets", basicAuth(getSets, user, pass))
-	router.GET("/deck/slack", makeSlackDeck)
+	router.GET("/slack/deck", makeSlackDeck)
 
 	dir, _ := os.Getwd()
 
