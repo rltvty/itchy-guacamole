fd3cdb678b2379e1ba09e46d8ea8faf247b826b8
AaronLaBrie <aaron@aaronlabrie.com>
2015-11-01 01:37:24 -0500

    Started on settings UI, bug fixes, updated package.json.

--- app/components/creator/Creator.jsx
+++ app/components/creator/Creator.jsx
@@ -4,11 +4,47 @@ var Deck = require('./Deck');
 var Loading = require('../Loading');
 var Error = require('../Error');
 var History = require('history');
+var Modal = require('react-modal');
 
 module.exports = React.createClass({
   displayName: 'Creator',
 
   componentDidMount: function() {
+    $.getJSON('/sets', function(data) {
+      this.setState({ availableSets: data });
+      this.getFirstDeck();
+    }.bind(this)).fail(function(err) {
+      this.handleError(err)
+    }.bind(this));
+  },
+
+  getInitialState: function() {
+    return {
+      loading: false,
+      error: false,
+      settingsOpen: false,
+      availableSets: [],
+      deck: {
+        cards: []
+      },
+      deckProperties: {
+        selectedSets: []
+      },
+      modalStyle: {
+        overlay : {
+          backgroundColor: 'rgba(175, 175, 175, 0.75)'
+        },
+        content: {
+          border: 'none',
+          background: 'none',
+          borderRadius: '4px',
+          padding: 'none'
+        }
+      }
+     };
+  },
+
+  getFirstDeck: function() {
     var id = this.getQueryParam("id");
     if(id) {
       this.getDeckByID(id);
@@ -17,16 +53,21 @@ module.exports = React.createClass({
     }
   },
 
-  getInitialState: function() {
-    return {
-      deck: { cards: [] },
-      loading: false,
-      error: false
-     };
+  showSettings: function() {
+    this.setState({settingsOpen: true});
   },
 
-  getParams: function() {
-    return { sets: this.refs.form.state.selectedSets };
+  hideSettings: function() {
+    this.setState({settingsOpen: false});
+  },
+
+  setDeckProperties: function(newProperties) {
+    this.setState({deckProperties: newProperties});
+  },
+
+  getJSONParams: function() {
+    var params = { sets: this.state.deckProperties.selectedSets }
+    return JSON.stringify(params);
   },
 
   getQueryParam: function(field) {
@@ -58,16 +99,15 @@ module.exports = React.createClass({
 
   getNewDeck: function() {
     this.setLoading();
-    var params = JSON.stringify(this.getParams());
+    var params = this.getJSONParams();
     $.post('/deck', params).success(function(data) {
       data = JSON.parse(data);
       this.setState({deck: data, loading: false});
+      this.updateHistory();
     }.bind(this))
     .error(function(err) {
       this.handleError(err);
     }.bind(this));
-
-    this.updateHistory();
   },
 
   updateHistory: function() {
@@ -81,8 +121,22 @@ module.exports = React.createClass({
   render: function() {
     return(
       <div>
-        <Form ref="form"/>
-        { this.state.loading ? <Loading /> :<Deck deck={this.state.deck} />}
+        <Modal isOpen={this.state.settingsOpen} onRequestClose={this.hideSettings} style={this.state.modalStyle}>
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title">Deck Settings</h5>
+            </div>
+            <div className="modal-body">
+              <Form sets={this.state.availableSets} deckProperties={this.state.deckProperties} setDeckProperties={this.setDeckProperties}/>
+            </div>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-sm btn-secondary" onClick={this.hideSettings}>Close</button>
+            </div>
+          </div>
+        </Modal>
+
+        { (!this.state.loading && !this.state.error) ? <Deck deck={this.state.deck}/> : null  }
+        { this.state.loading ? <Loading /> : null }
         { this.state.error ? <Error /> : null }
       </div>
     );
