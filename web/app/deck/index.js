b4d5cae0762eac27349adbcbb0f072e53a398649
AaronLaBrie <aaron@aaronlabrie.com>
2016-07-23 22:45:14 -0500

    Super messy api calls

--- web/app/deck/index.js
+++ web/app/deck/index.js
@@ -1,9 +1,26 @@
+import { Component } from 'react'
 import { connect } from 'react-redux'
+import { withRouter } from 'react-router'
+import fetch from 'isomorphic-fetch'
+
 import { Cards } from './cards'
+import { SetLoading, SetDeck } from '../actions'
 
 const defaultDeck = {
   cards: [],
   hardware: {}
+}
+
+const deckProperties = {
+  selectedSets: ['dominion'],
+  weights: {
+    trashing: 5,
+    random: 5,
+    chaining: 5,
+    cost_spread: 5,
+    set_count: 5,
+    mechanic_count: 5,
+  }
 }
 
 const DeckReducer = (state = defaultDeck, action) => {
@@ -16,36 +33,74 @@ const DeckReducer = (state = defaultDeck, action) => {
   }
 }
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = (state, {location}) => ({
   deck: state.deck,
+  id: location.query.id || '',
   error: state.error,
-  loading: state.loading,
+  loading: state.loading
 })
 
-const Error = () => (
-  <div id='error'>
-    <h2 className="text-danger text-center">An error occured! Try again!</h2>
-  </div>
-)
+const mapDispatchToProps = (dispatch) => ({
+  getDeck() {
+    getDeck(dispatch)
+  },
 
-const Loading = () => (
-  <div id='loading'>
-    <img src='/static/images/shield.png' className='fa fa-spin'></img>
-  </div>
-)
-
-const Deck = ({deck, error, loading}) => {
-  if (error) {
-    return <Error />
+  getDeckByID(id) {
+    getDeckByID(dispatch, id)
   }
+})
 
-  if (loading) {
-    <Loading />
-  }
+const getDeck = (dispatch) => {
+  dispatch(SetLoading(true))
 
-  return <Cards deck={deck} />
+  let body = JSON.stringify(deckProperties)
+  fetch('deck', {method: 'POST', body: body, headers: { Authorization: 'Basic '+btoa('user:password')}})
+    .then(res => res.json())
+    .then(deck => {
+      dispatch(SetDeck(deck))
+      dispatch(SetLoading(false))
+    })
 }
 
-const DeckContainer = connect(mapStateToProps)(Deck)
+const getDeckByID = (dispatch, id) => {
+  dispatch(SetLoading(true))
+  fetch(`deck/${id}`, { headers: { Authorization: 'Basic '+btoa('user:password')}})
+    .then(res => res.json())
+    .then(deck => {
+      dispatch(SetDeck(deck))
+      dispatch(SetLoading(false))
+    })
+}
 
-export { DeckContainer, Deck, Error, Loading, DeckReducer }
+class Deck extends Component {
+  componentDidMount() {
+    let { id, getDeck, getDeckByID } = this.props
+    id ? getDeckByID(id) : getDeck()
+  }
+
+  render() {
+    let { deck, error, loading } = this.props
+
+    if (error) {
+      return (
+        <div id='error'>
+          <h2 className="text-danger text-center">An error occured! Try again!</h2>
+        </div>
+      )
+    }
+
+    if (loading) {
+      return (
+        <div id='loading'>
+          <img src='/static/images/shield.png' className='fa fa-spin'></img>
+        </div>
+      )
+    }
+
+    return <Cards deck={deck} />
+  }
+}
+
+const DeckContainer = withRouter(connect(mapStateToProps, mapDispatchToProps)(Deck))
+
+export { DeckContainer, Deck, DeckReducer }
--- web/app/nav.js
+++ web/app/nav.js
@@ -1,11 +1,35 @@
 import { connect } from 'react-redux'
-import { NewDeck } from './actions'
+import { SetLoading, SetDeck } from './actions'
 
 const mapDispatchToProps = (dispatch) => ({
   newDeck() {
-    dispatch(NewDeck)
+    getDeck(dispatch)
   }
 })
+
+const deckProperties = {
+  selectedSets: ['dominion'],
+  weights: {
+    trashing: 5,
+    random: 5,
+    chaining: 5,
+    cost_spread: 5,
+    set_count: 5,
+    mechanic_count: 5,
+  }
+}
+
+const getDeck = (dispatch) => {
+  dispatch(SetLoading(true))
+
+  let body = JSON.stringify(deckProperties)
+  fetch('deck', {method: 'POST', body: body, headers: { Authorization: 'Basic '+btoa('user:password')}})
+    .then(res => res.json())
+    .then(deck => {
+      dispatch(SetDeck(deck))
+      dispatch(SetLoading(false))
+    })
+}
 
 const Nav = ({newDeck}) => (
   <div id='nav'>
